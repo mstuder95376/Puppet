800fe89986f390831ee3d1df55aab4e018abe3a8
Michael Studer <mike_studer@apple.com>
2018-05-18 09:51:21 -0700

    Split OS and User setup

--- my_OS_setup.pp
+++ my_OS_setup.pp
@@ -1,36 +1,11 @@
 # My new Linux system setup
-
+# OS setup should be run as root
 # First install my favorite packages
 class my_pkgs{
-    $my_packages = [ 'screen', 'tmux', 'vim', 'filezilla', 'meld', 'gcc', 'make', 'ansible', 'git', 'atom' ]
+    $my_packages = [ 'screen', 'tmux', 'vim', 'filezilla', 'meld', 'gcc', 'make', 'ansible', 'git', 'atom', 'openssh', 'glances', 'eclipse' ]
     package { $my_packages: ensure => 'installed' }
-}
-
-# Get my dot files downloaded and installed
-# Require a wget module
-# puppet module install maestrodev-wget
-class my_dirs{
-    file { '/home/mstuder/GitHub':
-        ensure => 'directory',
-    }
-}
-
-class my_gits{
-# https://github.com/mstuder95376/mydotfiles.git
-exec { 'mydotfiles':
-    command => 'git clone https://github.com/mstuder95376/mydotfiles.git /home/mstuder/GitHub/mydotfiles',
-    path    => '/usr/bin/:/bin/',
-    # path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
-  }
-  ->
-  exec { 'dotinstall':
-      command => 'installer.sh',
-      path    => '/home/mstuder/GitHub/mydotfiles/',
-    }
 }
 
 
 # To invoke these new classes
-#include my_pkgs
-#include my_dirs
-include my_gits
+include my_pkgs
---
+++ my_User_setup.pp
@@ -0,0 +1,42 @@
+# My new Linux system setup
+# User setup should be run as the user
+# =============== CONFIG ===============
+# Specifiy username here:
+$username = "mstuder"
+
+# =============== CLASSES ==============
+# Create GItHub directory if missing
+class my_dirs{
+    file { "/home/${username}/GitHub" :
+        ensure => 'directory',
+    }
+}
+
+class rm_mydotfiles {
+    file { "/home/${username}/GitHub/mydotfiles" :
+    ensure  => absent,
+    force => yes,
+    }
+}
+
+class my_gits{
+# https://github.com/mstuder95376/mydotfiles.git
+# Make sure directory is not already there.
+exec { 'mydotfiles' :
+    command => 'git clone https://github.com/mstuder95376/mydotfiles.git /home/mstuder/GitHub/mydotfiles',
+    path    => '/usr/bin/:/bin/',
+    # path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
+  }
+  ->
+  exec { 'dotinstall' :
+      command => 'installer.sh',
+      path    => "/home/${username}/GitHub/mydotfiles/",
+    }
+}
+
+
+# Invoke these new classes
+# ============ MAIN ============
+include my_dirs
+include rm_mydotfiles
+include my_gits
